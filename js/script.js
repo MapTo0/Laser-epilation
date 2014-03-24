7600a5c428a3d8a34e06f02b30afcdf0d62d967b
MapTo0 <martin.r.hristov@gmail.com>
2014-03-24 23:46:25 +0200

    more tests

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(window).load(function() {
     });
 });
 $(document).delegate("body", "pageinit", function() {
+	console.log(10);
     var url = window.location.search.substring(1);
     $('#slider').nivoSlider({
         controlNav: false
