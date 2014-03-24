b8076d5e932ac20133aaafa76692a38a181409c0
MapTo0 <martin.r.hristov@gmail.com>
2014-03-25 00:08:13 +0200

    jq .

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
 function linkTo() {
     window.location = "index.html";
 }
-$('[data-role=page]').live('pageshow', function(event, ui) {
-        $('#slider').nivoSlider({
-            controlNav: false
-        });
