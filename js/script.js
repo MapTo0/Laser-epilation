531ce6411af703187216271814a346691f199f89
MapTo0 <martin.r.hristov@gmail.com>
2014-03-24 23:45:17 +0200

    js test again

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ $(window).load(function() {
         controlNav: false
     });
 });
+$(document).delegate("body", "pageinit", function() {
+    var url = window.location.search.substring(1);
+    $('#slider').nivoSlider({
+        controlNav: false
+    });
+});
